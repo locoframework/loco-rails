571f12a1f24e69786ec86f0d26ebaf5e2338c5da
himn1 <zbigniew.humeniuk@gmail.com>
2016-08-31 13:02:41 +0200

    Fix

--- lib/generators/loco/js_assets_structure/js_assets_structure_generator.rb
+++ lib/generators/loco/js_assets_structure/js_assets_structure_generator.rb
@@ -2,6 +2,6 @@ class Loco::JsAssetsStructureGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def create_dir_struct
-    directory ".", "app/assets/javascripts/"
+    directory ".", File.join(Rails.root, 'app', 'assets', 'javascripts')
   end
 end
