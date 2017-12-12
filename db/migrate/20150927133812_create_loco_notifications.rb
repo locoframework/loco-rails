0d65a051e4eb01eb27ff7fcd14d9ee0b68f3dfd4
himn1 <zbigniew.humeniuk@gmail.com>
2017-12-12 11:46:34 +0100

    Fix for: StandardError: Directly inheriting from ActiveRecord::Migration is not supported.

--- db/migrate/20150927133812_create_loco_notifications.rb
+++ db/migrate/20150927133812_create_loco_notifications.rb
@@ -1,4 +1,4 @@
-class CreateLocoNotifications < ActiveRecord::Migration[4.2]
+class CreateLocoNotifications < ActiveRecord::Migration[5.0]
   def change
     create_table :loco_notifications do |t|
       t.string :obj_class
