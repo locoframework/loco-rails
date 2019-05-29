3a4434aa25ed97a568b4f897b6f8bc84864202fe
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-05-29 22:41:59 +0200

    loco.js config

--- test/dummy/frontend/js/initializers/loco.js
+++ test/dummy/frontend/js/initializers/loco.js
@@ -1,9 +1,23 @@
-import { Deps, Loco } from "loco-js";
+import { Deps, Env, Loco } from "loco-js";
 import ActionCable from "actioncable";
 import NotificationCenter from "services/notification_center.coffee";
 
 Deps.cable = ActionCable.createConsumer();
 Deps.NotificationCenter = NotificationCenter;
 
-const loco = new Loco();
+const loco = new Loco({
+  notifications: {
+    enable: true,
+    log: true,
+    size: 10
+  },
+  postInit: () => {
+    if (
+      document.querySelector("body").getAttribute("data-rails-env") !== "test"
+    )
+      return;
+    Env.loco.getWire().setPollingTime(1000);
+  }
+});
+
 loco.init();
