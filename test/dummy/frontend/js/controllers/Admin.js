0701cf7633d647629cc2bafb14e82bce7d122719
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-08-12 17:16:07 +0200

    1 file coffee -> js

--- test/dummy/frontend/js/controllers/Admin.js
+++ test/dummy/frontend/js/controllers/Admin.js
@@ -3,7 +3,7 @@ import { Controllers, Mix } from "loco-js";
 import Disconnection from "./concerns/disconnection.coffee";
 import Articles from "./admin/Articles";
 import Comments from "./admin/Comments";
-import Sessions from "./admin/sessions.coffee";
+import Sessions from "./admin/Sessions";
 import Users from "./admin/Users";
 
 class Admin extends Mix(Controllers.Base, Disconnection) {
---
+++ test/dummy/frontend/js/controllers/admin/Sessions.js
@@ -0,0 +1,11 @@
+import { Controllers } from "loco-js";
+
+import Form from "views/admin/sessions/form.coffee";
+
+class Sessions extends Controllers.Base {
+  new() {
+    new Form().render();
+  }
+}
+
+export default Sessions;
