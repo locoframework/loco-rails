76d31074183398bda35264d3dc9baaa9d0977824
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-08-25 13:59:06 +0200

    Fixed spec + Cable in async mode for tests

--- test/integration/user/direct_communication_test.rb
+++ test/integration/user/direct_communication_test.rb
@@ -13,7 +13,10 @@ class User::DirectCommunicationTest < IT
 
   test 'should show an alert' do
     sleep 0.1
+    perform_enqueued_jobs
     emit_to users(:user_zbig), signal: 'ping'
+    perform_enqueued_jobs
+    sleep 0.1
     assert_equal 'Ping!', page.driver.browser.switch_to.alert.text
     page.driver.browser.switch_to.alert.accept
   end
