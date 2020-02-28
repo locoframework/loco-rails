b3886834bb20d79b9a7697c7b8d370302f44552d
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-02-29 00:28:37 +0100

    [test] locales

--- test/dummy/frontend/js/locales/index.js
+++ test/dummy/frontend/js/locales/index.js
@@ -2,3 +2,4 @@ import "./base/pl";
 import "./models/user/pl";
 import "./validators/en.vulgarity";
 import "./validators/pl.vulgarity";
+import "./services/en.Date";
---
+++ test/dummy/frontend/js/locales/services/en.Date.js
@@ -0,0 +1,47 @@
+import { I18n } from "loco-js";
+
+I18n.en.date = {
+  formats: {
+    default: "%Y-%m-%d",
+    short: "%b %d",
+    long: "%B %d, %Y"
+  },
+  day_names: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+  abbr_day_names: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  month_names: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+  abbr_month_names: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ]
+};
