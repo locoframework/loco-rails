635fa803409a7c3cee2952959053515d7215a801
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-03-01 12:26:36 +0100

    [test] NotificationCenter is a function

--- test/dummy/frontend/js/services/NotificationCenter.js
+++ test/dummy/frontend/js/services/NotificationCenter.js
@@ -3,34 +3,30 @@ import { Env } from "loco-js";
 import UserController from "controllers/User";
 import RoomsController from "controllers/user/Rooms";
 
-class NotificationCenter {
-  receivedSignal(data) {
-    switch (data.signal) {
-      case "ping":
-        this._pingSignal();
-        break;
-      case "message":
-        this._messageSignal(data);
-    }
-  }
+const pingSignal = () => {
+  if (Env.namespaceController.constructor !== UserController) return;
+  alert("Ping!");
+};
 
-  _pingSignal() {
-    if (Env.namespaceController.constructor !== UserController) return;
-    alert("Ping!");
-  }
+const getRoomView = () => {
+  if (Env.namespaceController.constructor !== UserController) return false;
+  if (Env.controller.constructor !== RoomsController) return false;
+  if (Env.action !== "show") return false;
+  return Env.controller.getView("show");
+};
 
-  _messageSignal(data) {
-    const view = this._getRoomView();
-    if (!view) return;
-    view.receivedMessage(data.message, data.author);
-  }
+const messageSignal = data => {
+  const view = getRoomView();
+  if (!view) return;
+  view.receivedMessage(data.message, data.author);
+};
 
-  _getRoomView() {
-    if (Env.namespaceController.constructor !== UserController) return false;
-    if (Env.controller.constructor !== RoomsController) return false;
-    if (Env.action !== "show") return false;
-    return Env.controller.getView("show");
+export default data => {
+  switch (data.signal) {
+    case "ping":
+      pingSignal();
+      break;
+    case "message":
+      messageSignal(data);
   }
-}
-
-export default NotificationCenter;
+};
