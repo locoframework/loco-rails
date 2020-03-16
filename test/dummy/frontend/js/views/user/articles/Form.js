d440980d2e526eeb67cc04c5a0b37ce12eb03a22
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-03-16 17:18:12 +0100

    [test] connectWith => subscribe

--- test/dummy/frontend/js/views/user/articles/Form.js
+++ test/dummy/frontend/js/views/user/articles/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render as renderElement } from "react-dom";
-import { Views } from "loco-js";
+import { subscribe, Views } from "loco-js";
 import { UI } from "loco-js-ui";
 
 import { addArticles, setComments } from "actions";
@@ -12,21 +12,63 @@ import FlashView from "views/shared/Flash";
 
 import CommentList from "containers/user/CommentList";
 
+const displayChanges = article => {
+  for (const [attrib] of Object.entries(article.changes())) {
+    const sel = document.querySelector(
+      `a.apply_changes[data-for=${article.getAttrRemoteName(attrib)}]`
+    );
+    if (!sel) continue;
+    sel.classList.remove("none");
+  }
+};
+
+const createReceivedSignal = article => {
+  return async function(signal, data) {
+    switch (signal) {
+      case "updating":
+        if (
+          document.querySelector("h1").getAttribute("data-mark") !== data.mark
+        ) {
+          const flash = new FlashView({
+            warning: "Uuups someone else started editing this article."
+          });
+          flash.render();
+        }
+        break;
+      case "updated":
+        await article.reload();
+        displayChanges(article);
+        break;
+      case "destroyed":
+        window.location.href = "/user/articles?message=deleted";
+    }
+  };
+};
+
+const handleApplyingChanges = form => {
+  for (const sel of Array.from(document.querySelectorAll("a.apply_changes"))) {
+    sel.addEventListener("click", e => {
+      e.preventDefault();
+      const article = form.getObj();
+      const attrName = article.getAttrName(e.target.getAttribute("data-for"));
+      article[attrName] = article.changes()[attrName].is;
+      form.fill(attrName);
+      e.target.classList.add("none");
+    });
+  }
+};
+
 class Form extends Views.Base {
   constructor(opts = {}) {
     super(opts);
-    this.article = null;
-    this.form = null;
-    this.changes = null;
   }
 
   render(article) {
     store.dispatch(addArticles([article]));
-    this.article = article;
-    this.connectWith(this.article);
-    this._handleApplyingChanges();
-    this.form = new UI.Form({ for: this.article });
-    this.form.render();
+    subscribe({ to: article, with: createReceivedSignal(article) });
+    const form = new UI.Form({ for: article });
+    form.render();
+    handleApplyingChanges(form);
   }
 
   async renderComments(articleId) {
@@ -40,54 +82,6 @@ class Form extends Views.Base {
       />,
       document.getElementById("comments")
     );
-  }
-
-  async receivedSignal(signal, data) {
-    switch (signal) {
-      case "updating":
-        if (
-          document.querySelector("h1").getAttribute("data-mark") !== data.mark
-        ) {
-          const flash = new FlashView({
-            warning: "Uuups someone else started editing this article."
-          });
-          flash.render();
-        }
-        break;
-      case "updated":
-        await this.article.reload();
-        this.changes = this.article.changes();
-        this._displayChanges();
-        break;
-      case "destroyed":
-        window.location.href = "/user/articles?message=deleted";
-    }
-  }
-
-  _displayChanges() {
-    for (const [attrib] of Object.entries(this.changes)) {
-      const sel = document.querySelector(
-        `a.apply_changes[data-for=${this.article.getAttrRemoteName(attrib)}]`
-      );
-      if (!sel) continue;
-      sel.classList.remove("none");
-    }
-  }
-
-  _handleApplyingChanges() {
-    for (const sel of Array.from(
-      document.querySelectorAll("a.apply_changes")
-    )) {
-      sel.addEventListener("click", e => {
-        e.preventDefault();
-        const attrName = this.article.getAttrName(
-          e.target.getAttribute("data-for")
-        );
-        this.article[attrName] = this.changes[attrName].is;
-        this.form.fill(attrName);
-        e.target.classList.add("none");
-      });
-    }
   }
 }
 
