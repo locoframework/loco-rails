889054c9d939ccb95677ead4859d4d7d652ced7a
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-03-16 15:52:17 +0100

    connectWith => subscribe

--- test/dummy/frontend/js/views/main/users/UserRegistrationForm.js
+++ test/dummy/frontend/js/views/main/users/UserRegistrationForm.js
@@ -1,8 +1,28 @@
-import { Views } from "loco-js";
+import { subscribe, Views } from "loco-js";
 import { UI } from "loco-js-ui";
 
 import User from "models/User";
 import Flash from "views/shared/Flash";
+
+const confirming = () => {
+  document.getElementById(
+    "verification_info"
+  ).textContent = document.getElementById("verification_progress").textContent;
+};
+
+const confirmed = () => {
+  window.location.href = "/user/sessions/new?event=confirmed";
+};
+
+const receivedSignal = signal => {
+  switch (signal) {
+    case "confirming":
+      confirming();
+      break;
+    case "confirmed":
+      confirmed();
+  }
+};
 
 class UserRegistrationForm extends Views.Base {
   constructor(opts = {}) {
@@ -18,35 +38,13 @@ class UserRegistrationForm extends Views.Base {
     form.render();
   }
 
-  receivedSignal(signal) {
-    switch (signal) {
-      case "confirming":
-        this._confirming();
-        break;
-      case "confirmed":
-        this._confirmed();
-    }
-  }
-
   _created(data) {
-    this.connectWith(new User({ id: data.id }));
+    subscribe({ to: new User({ id: data.id }), with: receivedSignal });
     document.querySelector("form").style.display = "none";
     document.getElementById("sign_in_paragraph").classList.remove("none");
     document.getElementById("verification_info").classList.remove("none");
     const flash = new Flash({ notice: data.notice });
     flash.render();
-  }
-
-  _confirming() {
-    document.getElementById(
-      "verification_info"
-    ).textContent = document.getElementById(
-      "verification_progress"
-    ).textContent;
-  }
-
-  _confirmed() {
-    window.location.href = "/user/sessions/new?event=confirmed";
   }
 }
 
--- test/dummy/frontend/js/views/user/articles/Show.js
+++ test/dummy/frontend/js/views/user/articles/Show.js
@@ -1,6 +1,53 @@
-import { Views } from "loco-js";
+import { subscribe, Views } from "loco-js";
 
 import FlashView from "views/shared/Flash";
+
+const updateEditLink = id => {
+  const href = document.getElementById("edit_link").getAttribute("href");
+  document
+    .getElementById("edit_link")
+    .setAttribute("href", href.replace("/0/", `/${id}/`));
+};
+
+const handlePublishing = article => {
+  document
+    .getElementById("publish_article")
+    .addEventListener("click", async e => {
+      e.preventDefault();
+      e.target.textContent = "Publishing...";
+      try {
+        await article.put("publish");
+        document.getElementById("publish_article").outerHTML =
+          "<span>Published!</span>";
+      } catch (err) {
+        document.getElementById("publish_article").textContent = "Publish";
+        const flash = new FlashView({ alert: "Connection error!" });
+        flash.render();
+      }
+    });
+};
+
+const renderArticle = article => {
+  document.getElementById("article_title").textContent = article.title;
+  document.getElementById("article_text").textContent = article.content;
+  const node = document.getElementById("publish_article");
+  if (node && article.publishedAt != null) node.style.display = "none";
+  else if (node) node.style.display = "";
+};
+
+const createArticleReceivedSignal = article => {
+  return async function(signal) {
+    switch (signal) {
+      case "updated":
+        await article.reload();
+        article.applyChanges();
+        renderArticle(article);
+        break;
+      case "destroyed":
+        window.location.href = "/user/articles?message=deleted";
+    }
+  };
+};
 
 class Show extends Views.Base {
   constructor(opts = {}) {
@@ -9,54 +56,13 @@ class Show extends Views.Base {
   }
 
   render(article = null) {
-    if (this.article == null) {
-      this.connectWith(article, { receiver: "articleReceivedSignal" });
+    if (this.article === null) {
       this.article = article;
+      subscribe({ to: article, with: createArticleReceivedSignal(article) });
+      handlePublishing(article);
+      updateEditLink(article.id);
     }
-    document.getElementById("article_title").textContent = this.article.title;
-    document.getElementById("article_text").textContent = this.article.content;
-    const node = document.getElementById("publish_article");
-    if (node && this.article.publishedAt != null) node.style.display = "none";
-    else if (node) node.style.display = "";
-    this._handlePublishing();
-    this._updateEditLink();
-  }
-
-  async articleReceivedSignal(signal) {
-    switch (signal) {
-      case "updated":
-        await this.article.reload();
-        this.article.applyChanges();
-        this.render();
-        break;
-      case "destroyed":
-        window.location.href = "/user/articles?message=deleted";
-    }
-  }
-
-  _handlePublishing() {
-    document
-      .getElementById("publish_article")
-      .addEventListener("click", async e => {
-        e.preventDefault();
-        e.target.textContent = "Publishing...";
-        try {
-          await this.article.put("publish");
-          document.getElementById("publish_article").outerHTML =
-            "<span>Published!</span>";
-        } catch (err) {
-          document.getElementById("publish_article").textContent = "Publish";
-          const flash = new FlashView({ alert: "Connection error!" });
-          flash.render();
-        }
-      });
-  }
-
-  _updateEditLink() {
-    const href = document.getElementById("edit_link").getAttribute("href");
-    document
-      .getElementById("edit_link")
-      .setAttribute("href", href.replace("/0/", `/${this.article.id}/`));
+    renderArticle(this.article);
   }
 }
 
