a54ae89ed9b07d05ae48d5693fb30197b654429b
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-03-29 00:21:59 +0100

    [test] refactoring

--- test/dummy/frontend/js/initializers/loco.js
+++ test/dummy/frontend/js/initializers/loco.js
@@ -1,4 +1,4 @@
-import { Env, connector as locoConnector, init } from "loco-js";
+import { getWire, connector as locoConnector, init } from "loco-js";
 import { connect as connectUI } from "loco-js-ui";
 import { createConsumer } from "@rails/actioncable";
 import NotificationCenter from "services/NotificationCenter";
@@ -16,7 +16,7 @@ init({
       document.querySelector("body").getAttribute("data-rails-env") !== "test"
     )
       return;
-    Env.loco.wire.setPollingTime(1000);
+    getWire().setPollingTime(1000);
   }
 });
 
