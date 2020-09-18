6d02288f70c4f36db5b02350f3d10ec731b256c7
himn1 <hello@artofcode.co>
2020-09-18 12:04:16 +0200

    [test]

--- test/loco/ws_connection_manager_test.rb
+++ test/loco/ws_connection_manager_test.rb
@@ -11,9 +11,19 @@ module Loco
     end
 
     describe '#identifier' do
-      it do
+      it 'returns a correct format of identifier' do
         assert_equal "user:#{@user.id}", @subject.identifier
         assert_equal 'foo', @described_class.new('foo').identifier
+      end
+    end
+
+    describe '#add' do
+      it 'returns a correct structure' do
+        random = SecureRandom.uuid
+        expected = { random => '2020-01-01T00:00:00.000000Z' }
+        travel_to Time.utc(2020) do
+          assert_equal expected, JSON.parse(@subject.add(random))
+        end
       end
     end
   end
