8bb2ae0b0de65ec5c09a0f5ebf1ce6912aab81b9
himn1 <hello@artofcode.co>
2020-09-21 10:59:35 +0200

    [test]

--- test/loco/ws_connection_manager_test.rb
+++ test/loco/ws_connection_manager_test.rb
@@ -13,6 +13,27 @@ module Loco
       reset_connections(@user)
     end
 
+    describe 'checking connections' do
+      it 'is run after add and del' do
+        uuid1 = SecureRandom.uuid
+        uuid2 = SecureRandom.uuid
+        uuid3 = SecureRandom.uuid
+        time = Time.utc(2020, 0o1, 0o1, 11, 30)
+        travel_to(time) { @subject.add(uuid1) }
+        travel_to(time + (3 * 60 - 1).seconds) { @subject.add(uuid2) }
+        assert_equal '2020-01-01T11:30:00.000000Z', @subject.send(:data)[uuid1]
+
+        payload = { loco: { connection_check: true } }
+        expect(SenderJob).to receive(:perform_later).with(uuid1, payload)
+        travel_to(time + (3 * 60 + 1).seconds) { @subject.add(uuid3) }
+        assert_equal({ 'check' => '2020-01-01T11:33:01.000000Z' }, @subject.send(:data)[uuid1])
+        assert_equal 3, @subject.send(:data).keys.size
+
+        travel_to(time + (3 * 60 + 1 + 6).seconds) { @subject.del(uuid2) }
+        assert_equal({ uuid3 => '2020-01-01T11:33:01.000000Z' }, @subject.send(:data))
+      end
+    end
+
     describe '#add' do
       it 'adds UUID and it is considered connected' do
         uuid = SecureRandom.uuid
@@ -20,18 +41,6 @@ module Loco
           @subject.add(uuid)
         end
         assert @subject.connected?(uuid)
-      end
-
-      it 'checks connections' do
-        uuid = SecureRandom.uuid
-        time = Time.utc(2020, 0o1, 0o1, 11, 30)
-        travel_to(time) { @subject.add(uuid) }
-        travel_to(time + (3 * 60 - 1).seconds) { @subject.add(SecureRandom.uuid) }
-        assert_equal '2020-01-01T11:30:00.000000Z', @subject.send(:data)[uuid]
-        expect(SenderJob).to receive(:perform_later).with(uuid, loco: { connection_check: true })
-        travel_to(time + (3 * 60 + 1).seconds) { @subject.add(SecureRandom.uuid) }
-        assert_equal({ 'check' => '2020-01-01T11:33:01.000000Z' }, @subject.send(:data)[uuid])
-        assert_equal 3, @subject.send(:data).keys.size
       end
     end
 
