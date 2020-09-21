c4cf66f4e7e3fe74eb25bbb55061470a5920eba6
himn1 <hello@artofcode.co>
2020-09-21 07:25:44 +0200

    [test]

--- test/loco/sender_test.rb
+++ test/loco/sender_test.rb
@@ -3,10 +3,27 @@
 require 'test_helper'
 
 module Loco
-  class SenderTest < ActiveSupport::TestCase
+  class SenderTest < TCWithMocks
     describe '#emit' do
       it 'returns idempotency_key' do
-        assert_not_nil Sender.new(users(:zbig), {}).emit
+        key = SecureRandom.hex
+        assert_equal key, Sender.new(users(:zbig), { idempotency_key: key }).emit
+      end
+
+      it 'sends a passed idempotency key' do
+        uuid = SecureRandom.uuid
+        key = SecureRandom.hex
+        payload = { loco: { idempotency_key: key } }
+        expect(NotificationCenterChannel).to receive(:broadcast_to).with(uuid, payload)
+        Sender.new(uuid, { idempotency_key: key }).emit
+      end
+
+      it 'sends a generated idempotency key' do
+        uuid = SecureRandom.uuid
+        expect(SecureRandom).to receive(:hex) { 'foobarbaz' }
+        payload = { loco: { idempotency_key: 'foobarbaz' } }
+        expect(NotificationCenterChannel).to receive(:broadcast_to).with(uuid, payload)
+        Sender.new(uuid, {}).emit
       end
     end
   end
