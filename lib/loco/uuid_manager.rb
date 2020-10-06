2b7020702e45eac7df366ac201f27cdcbf991c31
himn1 <hello@artofcode.co>
2020-10-06 11:49:51 +0200

    [internal] refactoring

--- lib/loco/uuid_manager.rb
+++ lib/loco/uuid_manager.rb
@@ -4,28 +4,9 @@ module Loco
   class UuidManager
     class << self
       def dispatch(action, uuid, ws_conn_manager)
-        case action
-        when 'add'
-          add(ws_conn_manager, uuid)
-        when 'del'
-          del(ws_conn_manager, uuid)
-        when 'update'
-          update(ws_conn_manager, uuid)
-        end
-      end
+        return unless %w[add del update].include?(action)
 
-      private
-
-      def add(ws_conn_manager, uuid)
-        ws_conn_manager.add(uuid)
-      end
-
-      def del(ws_conn_manager, uuid)
-        ws_conn_manager.del(uuid)
-      end
-
-      def update(ws_conn_manager, uuid)
-        ws_conn_manager.update(uuid)
+        ws_conn_manager.public_send(action, uuid)
       end
     end
   end
