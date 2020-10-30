39e7739d1b60206a13fddca049919be1a720cc8c
himn1 <hello@artofcode.co>
2020-10-30 11:05:57 +0100

    [internal] refactoring

--- app/jobs/loco/uuid_job.rb
+++ app/jobs/loco/uuid_job.rb
@@ -5,13 +5,15 @@ module Loco
     queue_as :loco
 
     def perform(serialized_resource, uuid, action)
+      return unless %w[add del update].include?(action)
+
       resource = Jobs::ResourceSerializer.deserialize(serialized_resource)
       return unless resource
 
       ws_conn_manager = WsConnectionManager.new(resource)
       return unless ws_conn_manager
 
-      UuidManager.dispatch(action, uuid, ws_conn_manager)
+      ws_conn_manager.public_send(action, uuid)
     end
   end
 end
