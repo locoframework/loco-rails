ac20d4a0c60a4bf0762b166ca1dc6e4993fbd15b
himn1 <hello@artofcode.co>
2020-11-09 16:55:23 +0100

    [test] refactoring

--- test/loco/ws_connection_storage_test.rb
+++ test/loco/ws_connection_storage_test.rb
@@ -58,15 +58,15 @@ module Loco
     describe '#scan' do
       before do
         @storage.set('key1', 'UUID#1' => '12345')
-        @storage.set('user:980204181', 'UUID#2' => '22345')
-        @storage.set('admin:980190962', 'UUID#3' => '32345')
-        @storage.set('admin:880190960', 'UUID#4' => '42345')
-        @storage.set('comment:980190961', 'UUID#5' => '52345')
+        @storage.set('key2', 'UUID#2' => '22345')
+        @storage.set('key31', 'UUID#3' => '32345')
+        @storage.set('key32', 'UUID#4' => '42345')
+        @storage.set('key5', 'UUID#5' => '52345')
       end
 
       it 'accepts pattern' do
         res = {}
-        @storage.scan(match: 'admin:*') { |k, v| res[k] = v }
+        @storage.scan(match: 'key3*') { |k, v| res[k] = v }
         assert_equal({ 'UUID#3' => '32345', 'UUID#4' => '42345' }, res)
       end
     end
