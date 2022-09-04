623d48b030307819ea9fa6577112751830483f5f
himn1 <hello@artofcode.co>
2022-09-04 21:32:37 +0200

    [test] bump loco-js

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -13,8 +13,6 @@ require_relative 'boot'
   require railtie
 end
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 require 'sprockets/railtie'
