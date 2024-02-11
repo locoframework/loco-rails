bc6daa6ff6160e4a2cc9afec5e70b7d53f09b341
Zbig <hello@artofcode.co>
2024-02-12 00:36:32 +0100

    update test

--- test/integration/user/article_edit_page_test.rb
+++ test/integration/user/article_edit_page_test.rb
@@ -29,9 +29,9 @@ class User
     end
 
     test 'should allow to update fields in order to have updated values' do
-      sleep 1
+      sleep 0.1
       update_article :two
-      sleep 1
+      sleep 0.1
       within "div[data-attr='title']" do
         click_link 'apply changes'
         update_value_attr_for '#article_title'
