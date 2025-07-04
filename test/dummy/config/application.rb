e75ff8246791befdeb9a39cf97bd87befad4a1eb
himn1 <hello@artofcode.co>
2025-07-04 16:58:51 +0200

    Rails 8

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,17 +2,22 @@
 
 require_relative 'boot'
 
-%w[
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  active_job/railtie
-  action_cable/engine
-  rails/test_unit/railtie
-].each do |railtie|
-  require railtie
-end
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+# require "active_storage/engine"
+require 'action_controller/railtie'
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'rails/test_unit/railtie'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 require 'sprockets/railtie'
@@ -23,6 +28,20 @@ require 'redis'
 
 module Dummy
   class Application < Rails::Application
-    config.load_defaults 7.2
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 8.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
