a19bd720884856106dbe607541b44635705b43bf
himn1 <hello@artofcode.co>
2025-08-19 05:55:09 +0200

    deprecate sync_time

--- app/controllers/loco/notification_center_controller.rb
+++ app/controllers/loco/notification_center_controller.rb
@@ -10,18 +10,20 @@ module Loco
       end
     end
 
+    # TODO: delete in the next release
     def sync_time
+      ActiveSupport::Deprecation.warn(
+        'Loco::NotificationCenterController#sync_time is deprecated and will be removed in the next release.',
+        caller_locations(1)
+      )
       render json: { sync_time: Time.current.iso8601(6) }
     end
 
     private
 
     def fetch_notifications
-      if params[:synced_at].blank?
-        render json: [[], Time.current.iso8601(6)]
-        return
-      end
-      fetcher = Notification::Fetcher.new({ synced_at: params[:synced_at],
+      synced_at = params[:synced_at] || Time.current.iso8601(6)
+      fetcher = Notification::Fetcher.new({ synced_at:,
                                             permissions:,
                                             recipient_token: params[:token] })
       render json: [fetcher.formatted_notifications, fetcher.next_sync_time.iso8601(6)]
