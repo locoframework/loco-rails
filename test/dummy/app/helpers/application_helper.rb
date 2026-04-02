1415c0b37f6480829087345cf96a50eec9f510b7
himn1 <hello@artofcode.co>
2026-04-02 06:15:43 +0200

    no loco-rails-core

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  include Loco::Core::Helpers
+  def simplicit_body_attrs
+    namespace = controller_path
+                .split('/')
+                .then { |parts| parts[0...-1] } # everything except the controller name
+                .map(&:camelize)
+                .join('/')
+    {
+      data: {
+        namespace: namespace.presence,           # -> data-namespace="Main/Panel"
+        controller: controller_name.camelize,    # -> data-controller="Articles"
+        action: action_name                      # -> data-action="index"
+      }.compact
+    }
+  end
+
   def admin_panel?
     controller_path.split('/').first == 'admin'
   end
