7f1c6eb55b61710774e896752763a1c13997ef4f
himn1 <hello@artofcode.co>
2026-04-07 05:04:57 +0200

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Loco::Engine.routes.draw do
-  get 'sync-time' => 'notification_center#sync_time' # TODO: delete in the next release
+  get 'sync-time' => 'notification_center#sync_time' # TODO: delete in the 7.1 release
   root 'notification_center#index'
 end
